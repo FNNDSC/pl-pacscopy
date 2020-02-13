21ef5ef6fce4ff3cfa184a3351af1b52d9b400fc
Jorge <jbernal0019@yahoo.es>
2020-02-12 19:48:59 -0500

    v1.0.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setup(
       name             =   'pacscopy',
-      version          =   '1.0.4',
+      version          =   '1.0.5',
       description      =   'copy from (openstorage) to output directory', 
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
